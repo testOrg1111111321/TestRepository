92c88ea9717b58f396ca933dbe36f6f61a9a9f58
kozluck <kozluck@icloud.com>
2021-06-22 03:05:28 +0200

    Initial commit

---
+++ src/main/java/com/kozluck/EmployeesApp/EmployeesAppApplication.java
@@ -0,0 +1,13 @@
+package com.kozluck.EmployeesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesAppApplication.class, args);
+	}
+
+}
